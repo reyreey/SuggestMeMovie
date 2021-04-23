e6a11ba3fc6298ce64d332dd009e447a91edd50b
Reyhaneh <reyhaneh179@yahoo.com>
2021-04-23 12:11:47 +0430

    add MovieFinder app to INSTALED_APPS add OMDB api_key to settings

--- SuggestMeMovie/settings.py
+++ SuggestMeMovie/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MovieFinder.apps.MoviefinderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# omdb api
+OMDB_URL='http://www.omdbapi.com/'
+OMDB_API_KEY='3fa43373'
