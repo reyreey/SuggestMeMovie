46d2fbdab6d36e5be4640a3e1b7d9362fcbf8e7f
Reyhaneh <reyhaneh179@yahoo.com>
2021-04-23 12:24:08 +0430

    add models to admin panel

--- MovieFinder/admin.py
+++ MovieFinder/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from MovieFinder.models import *
+
+admin.site.register(TitleBasics)
+admin.site.register(TitleAkas)
+admin.site.register(NameBasics)
+admin.site.register(TitleCrew)
+admin.site.register(TitleEpisode)
+admin.site.register(TitleRatings)
+admin.site.register(Poster)
